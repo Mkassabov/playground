f164cf08c75212c3cd2a0914d0e209d09294f8cb
Michael K <35264484+Mkassabov@users.noreply.github.com>
2025-08-05 14:28:19 +0800

    stuff

--- alchemy.run.ts
+++ alchemy.run.ts
@@ -1,17 +1,25 @@
 import alchemy from "alchemy";
-// import { Worker } from "alchemy/cloudflare";
-import { File } from "alchemy/fs";
+import { DurableObjectNamespace, Worker } from "alchemy/cloudflare";
+import type { Counter } from "./deployments/alchemy-worker/src/patpat";
 
+// import { File } from "alchemy/fs";
+
+const counter = DurableObjectNamespace<Counter>("Counter", {
+	className: "Counter",
+	sqlite: true,
+});
 export const app = await alchemy("alchemy-test", {
 	stage: "dev",
 	phase: process.argv.includes("--destroy") ? "destroy" : "up",
 	password: process.env.ALCHEMY_PASSWORD,
-	// telemetry: false,
+	telemetry: false,
 });
 
-await File("my-file", {
-	path: "./test.txt",
-	content: "Hello, world!",
+export const worker = await Worker("patpat", {
+	script: "./deployments/alchemy-worker/src/patpat.ts",
+	bindings: {
+		COUNTER: counter,
+	},
 });
 
 await app.finalize();
--- deployments/alchemy-worker/src/taptap.ts
+++
@@ -1,7 +0,0 @@
-export default {
-	// biome-ignore lint/suspicious/useAwait: <explanation>
-	async fetch(_request: Request): Promise<Response> {
-		console.log("hai - taptap");
-		return new Response("Hello taptap");
-	},
-};

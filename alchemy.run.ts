5a5182fff756478c6f472e6f28d628bc666cda8a
Michael K <35264484+Mkassabov@users.noreply.github.com>
2025-09-16 14:25:39 -0400

    stuff

--- alchemy.run.ts
+++ alchemy.run.ts
@@ -1,5 +1,5 @@
 import alchemy from "alchemy";
-import { Worker, WorkerRef, Self } from "alchemy/cloudflare";
+import { Worker, WorkerRef, Self, WranglerJson } from "alchemy/cloudflare";
 import type { TapTapEntrypoint } from "./deployments/alchemy-worker/src/taptap";
 import type { PatPatEntrypoint } from "./deployments/alchemy-worker/src/patpat";
 
@@ -27,7 +27,19 @@ export const patpat = await Worker("patpat", {
 			"TapTapEntrypoint",
 		),
 		SELF: Worker.entrypoint<PatPatEntrypoint>(Self, "PatPatEntrypoint"),
+		RAWTAPTAP: taptap,
+		RAWTAPTAPREF: taptapRef,
+		RAWSELF: Self,
 	},
+});
+
+export const wranglerJsonTaptap = await WranglerJson({
+	worker: taptap,
+	path: "wrangler.taptap.jsonc",
+});
+export const wranglerJsonPatpat = await WranglerJson({
+	worker: patpat,
+	path: "wrangler.patpat.jsonc",
 });
 
 await app.finalize();

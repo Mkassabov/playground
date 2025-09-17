f040eb7236d05ba4b2d121e412733ca17c25659b
Michael K <35264484+Mkassabov@users.noreply.github.com>
2025-09-17 12:44:39 -0400

    queue destory testing

--- alchemy.run.ts
+++ alchemy.run.ts
@@ -1,13 +1,45 @@
 import alchemy from "alchemy";
-import { Worker } from "alchemy/cloudflare";
+import { Worker, Queue } from "alchemy/cloudflare";
 
 export const app = await alchemy("alchemy-test", {
 	telemetry: false,
 });
 
+const queueAName = `queue-a-${app.stage}`;
+const queueA = await Queue(queueAName, {
+	name: queueAName,
+	adopt: true,
+	delete: true,
+});
+
+const queueBName = `queue-b-${app.stage}`;
+const queueB = await Queue(queueBName, {
+	name: queueBName,
+	adopt: true,
+	delete: true,
+});
+
 export const patpat = await Worker("patpat", {
 	entrypoint: "./deployments/alchemy-worker/src/patpat.ts",
 	url: true,
+	bindings: {
+		QUEUE_A: queueA,
+		QUEUE_B: queueB,
+	},
+	eventSources: [
+		{
+			queue: queueA,
+			settings: { batchSize: 10, maxWaitTimeMs: 60 * 1000 },
+			adopt: true,
+			delete: true,
+		},
+		{
+			queue: queueB,
+			settings: { batchSize: 10, maxWaitTimeMs: 60 * 1000 },
+			adopt: true,
+			delete: true,
+		},
+	],
 });
 
 await app.finalize();
--- deployments/alchemy-worker/src/patpat.ts
+++ deployments/alchemy-worker/src/patpat.ts
@@ -1,4 +1,5 @@
 import { id as nanoid } from "../../../utils/nanoid";
+import type { patpat } from "../../../alchemy.run";
 
 export default {
 	// biome-ignore lint/suspicious/useAwait: its fine
@@ -7,5 +8,16 @@ export default {
 		const message = `Haiii - patpat! ${id}`;
 		console.log(message);
 		return new Response(message);
+	},
+
+	// biome-ignore lint/suspicious/useAwait: its fine
+	async queue(
+		batch: MessageBatch,
+		_env: typeof patpat.Env,
+		_ctx: ExecutionContext,
+	): Promise<void> {
+		for (const message of batch.messages) {
+			console.log("Received", message);
+		}
 	},
 };

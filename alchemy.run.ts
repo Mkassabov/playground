3f5d0bf3761411c41de833799463f150423a9f38
Michael K <35264484+Mkassabov@users.noreply.github.com>
2025-10-02 16:25:41 -0400

    secret stuff

--- alchemy.run.ts
+++ alchemy.run.ts
@@ -1,11 +1,11 @@
 import alchemy from "alchemy";
 import { Worker } from "alchemy/cloudflare";
 
-export const app = await alchemy("alchemy-test", {
+export const app = await alchemy("alchemy-try-test", {
 	telemetry: false,
 });
 
-export const patpat = await Worker("patpat", {
+export const patpat = await Worker("made-by-alchemy-test", {
 	entrypoint: "./deployments/alchemy-worker/src/patpat.ts",
 	url: true,
 });

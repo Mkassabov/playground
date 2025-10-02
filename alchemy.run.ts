18d2021996cd86c73e7dd1e393787ab7d986c4ac
Michael K <35264484+Mkassabov@users.noreply.github.com>
2025-10-02 16:28:54 -0400

    try 2

--- alchemy.run.ts
+++ alchemy.run.ts
@@ -1,9 +1,7 @@
 import alchemy from "alchemy";
 import { Worker } from "alchemy/cloudflare";
 
-export const app = await alchemy("alchemy-try-test", {
-	telemetry: false,
-});
+export const app = await alchemy("alchemy-try-test");
 
 export const patpat = await Worker("made-by-alchemy-test", {
 	entrypoint: "./deployments/alchemy-worker/src/patpat.ts",

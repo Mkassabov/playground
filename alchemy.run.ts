29d53d6b32533b96b8fe1914f6b57b60ece454d7
Michael K <35264484+Mkassabov@users.noreply.github.com>
2025-10-28 17:29:30 -0400

    CLEAN

--- alchemy.run.ts
+++ alchemy.run.ts
@@ -1,11 +1,11 @@
 import alchemy from "alchemy";
 import { Worker } from "alchemy/cloudflare";
 
-export const app = await alchemy("alchemy-try-test");
+export const app = await alchemy("alchemy-try-test", {});
 
-export const patpat = await Worker("made-by-alchemy-test", {
-	entrypoint: "./deployments/alchemy-worker/src/patpat.ts",
-	url: true,
+export const patpat = await Worker("made-by-alchemy-test-2", {
+  entrypoint: "./deployments/alchemy-worker/src/patpat.ts",
+  url: true,
 });
 
 await app.finalize();
--- utils/nanoid.ts
+++ utils/nanoid.ts
@@ -5,39 +5,39 @@ let pool: Uint8Array;
 let poolOffset: number;
 
 function fillPool(bytes: number) {
-	if (!pool || pool.length < bytes) {
-		pool = new Uint8Array(bytes * POOL_SIZE_MULTIPLIER);
-		crypto.getRandomValues(pool);
-		poolOffset = 0;
-	} else if (poolOffset + bytes > pool.length) {
-		crypto.getRandomValues(pool);
-		poolOffset = 0;
-	}
-	poolOffset += bytes;
+  if (!pool || pool.length < bytes) {
+    pool = new Uint8Array(bytes * POOL_SIZE_MULTIPLIER);
+    crypto.getRandomValues(pool);
+    poolOffset = 0;
+  } else if (poolOffset + bytes > pool.length) {
+    crypto.getRandomValues(pool);
+    poolOffset = 0;
+  }
+  poolOffset += bytes;
 }
 
 export function idGenerator(alphabet: string, defaultSize: number) {
-	const mask = (2 << (31 - Math.clz32((alphabet.length - 1) | 1))) - 1;
-	const step = Math.ceil((1.6 * mask * defaultSize) / alphabet.length);
+  const mask = (2 << (31 - Math.clz32((alphabet.length - 1) | 1))) - 1;
+  const step = Math.ceil((1.6 * mask * defaultSize) / alphabet.length);
 
-	return (size = defaultSize) => {
-		let id = "";
-		while (true) {
-			fillPool(step);
-			const bytes = pool.subarray(poolOffset - step, poolOffset);
-			for (let i = 0; i < step; i++) {
-				// biome-ignore lint/style/noNonNullAssertion: we iterate down from step. will always exist
-				id += alphabet[bytes[i]! & mask] || "";
-				if (id.length === size) {
-					return id;
-				}
-			}
-		}
-	};
+  return (size = defaultSize) => {
+    let id = "";
+    while (true) {
+      fillPool(step);
+      const bytes = pool.subarray(poolOffset - step, poolOffset);
+      for (let i = 0; i < step; i++) {
+        // biome-ignore lint/style/noNonNullAssertion: we iterate down from step. will always exist
+        id += alphabet[bytes[i]! & mask] || "";
+        if (id.length === size) {
+          return id;
+        }
+      }
+    }
+  };
 }
 
 export const DEFAULT_ALPHABET =
-	"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 export const DEFAULT_LENGTH = 10;
 export const id = idGenerator(DEFAULT_ALPHABET, DEFAULT_LENGTH);
 

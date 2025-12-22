417f9a174952dbf59dfdff6becc41c782e591f71
Michael K <35264484+Mkassabov@users.noreply.github.com>
2025-12-22 10:45:29 -0500

    bsaic

--- alchemy.run.ts
+++ alchemy.run.ts
@@ -1,11 +1,16 @@
 import alchemy from "alchemy";
-import { Worker } from "alchemy/cloudflare";
+import { Worker, WranglerJson } from "alchemy/cloudflare";
 
-export const app = await alchemy("alchemy-try-test", {});
-
-export const patpat = await Worker("made-by-alchemy-test-2", {
-  entrypoint: "./deployments/alchemy-worker/src/patpat.ts",
-  url: true,
+export const app = await alchemy("app", {
+  password: "test",
 });
+
+const worker = await Worker("form-builder", {
+  entrypoint: "./deployments/alchemy-worker/src/patpat.ts",
+});
+
+await WranglerJson({ worker });
+
+console.log(worker);
 
 await app.finalize();

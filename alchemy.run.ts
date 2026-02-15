c598fe9f765e185bdcc072540cc4e52e1fd173e4
Michael K <35264484+Mkassabov@users.noreply.github.com>
2026-02-15 15:24:40 -0500

    basic

--- alchemy.run.ts
+++ alchemy.run.ts
@@ -1,12 +1,16 @@
 import alchemy from "alchemy";
 import { Worker, WranglerJson, Hyperdrive } from "alchemy/cloudflare";
 
-export const app = await alchemy("app", {
+export const app = await alchemy("app-oauth", {
   password: "test",
 });
 
 const worker = await Worker("patpat", {
   entrypoint: "./deployments/alchemy-worker/src/patpat.ts",
+});
+
+await WranglerJson({
+  worker,
 });
 
 console.log(worker);
--- deployments/alchemy-worker/src/patpat.ts
+++ deployments/alchemy-worker/src/patpat.ts
@@ -1,12 +1,9 @@
 import { id as nanoid } from "../../../utils/nanoid";
 
-// work here is OUTSIDE the request scope
-const outsideScope = nanoid();
 
 export default {
   async fetch(): Promise<Response> {
-    // work here is INSIDE the request scope
-    const insideScope = nanoid();
-    return new Response({ outsideScope, insideScope });
+    const reqid = nanoid();
+    return new Response(JSON.stringify({ id:reqid, "test": "panda"  }));
   },
 };

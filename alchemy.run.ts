0b5219cae30b665656a0314cba690f6dd180f73b
Michael K <35264484+Mkassabov@users.noreply.github.com>
2026-03-05 09:28:31 -0500

    chore: idk update p2

--- alchemy.run.ts
+++ alchemy.run.ts
@@ -1,8 +1,14 @@
 import alchemy from "alchemy";
-import { Tunnel } from "alchemy/cloudflare";
+import { Worker } from "alchemy/cloudflare";
 
 export const app = await alchemy("test-app", {
   password: "test",
 });
+
+const worker = await Worker("patpat", {
+  entrypoint: "./deployments/alchemy-worker/src/patpat.ts",
+});
+
+console.log(worker)
 
 await app.finalize();
